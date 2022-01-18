8709e3347545db92235dac0dec8fecfc4e85c2b3
stormybytes <stormybytes@gmail.com>
2022-01-19 06:30:40 +0700

    Fixed bug with decompiler

--- decompile/decompile.py
+++ decompile/decompile.py
@@ -6,17 +6,14 @@ from pprint import pprint
 jumps = {}
 FILE_NAME = "input.txt"
 JUMP_REGEX = r"jump (\d+)"
-with open(FILE_NAME, "r") as file:
-    for line in file:
+with open(FILE_NAME, "r") as inFile:
+    for line in inFile:
         if re.search(JUMP_REGEX, line):
             jumps[re.search(JUMP_REGEX, line).group(1)] = ''.join(random.choices(string.ascii_lowercase + string.ascii_uppercase, k=8))
-with open(FILE_NAME, "r") as f:
-    text = f.read()
 
-
-with open ("output.txt", "r+") as file2:
-    with open(FILE_NAME, "r") as file:
-        for line in file:
+with open ("output.txt", "w") as outFile:
+    with open(FILE_NAME, "r") as inFile:
+        for line in inFile:
             if re.search(r"jump \d+", line):
                 search = re.search(r"jump (\d+) (.+)\s", line)
                 replace = "jump "
@@ -24,10 +21,9 @@ with open ("output.txt", "r+") as file2:
                 replace += " "
                 replace += search.group(2)
                 replace += "\n"
-
-                file2.write(replace)
+                outFile.write(replace)
             else:
-                file2.write(line)
+                outFile.write(line)
 
 with open('output.txt', 'r+') as f:
     lines = f.readlines()
