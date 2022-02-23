f58b0bb024ac15c799b54ecc40505bb3ae9236db
stormybytes <stormybytes@gmail.com>
2022-02-23 20:31:32 +0700

    "Fixed" mindustry custom symbols on decompile.py

--- decompile/decompile.py
+++ decompile/decompile.py
@@ -6,14 +6,14 @@ from pprint import pprint
 jumps = {}
 FILE_NAME = "input.txt"
 JUMP_REGEX = r"jump (\d+)"
-with open(FILE_NAME, "r") as inFile:
+with open(FILE_NAME, "r", errors="replace") as inFile:
     for line in inFile:
         if re.search(JUMP_REGEX, line):
             jumps[re.search(JUMP_REGEX, line).group(1)] = ''.join(random.choices(string.ascii_lowercase + string.ascii_uppercase, k=8))
 
 
-with open ("output.txt", "w") as outFile:
-    with open(FILE_NAME, "r") as inFile:
+with open("output.txt", "w", errors="replace") as outFile:
+    with open(FILE_NAME, "r", errors="replace") as inFile:
         for line in inFile:
             if re.search(r"jump \d+", line):
                 search = re.search(r"jump (\d+) (.+)\s", line)
@@ -26,7 +26,7 @@ with open ("output.txt", "w") as outFile:
             else:
                 outFile.write(line)
 
-with open('output.txt', 'r+') as f:
+with open('output.txt', 'r+', errors="replace") as f:
     lines = f.readlines()
     for i, line in enumerate(lines):
         if str(i + 1) in jumps:
